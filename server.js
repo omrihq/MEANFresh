884dc385ea88831d6be851ce4753607bc3a68cea
omrihq <omrihq@gmail.com>
2015-10-25 23:29:02 -0700

    cronjobs working on server, able to scrape and log songs once a minute

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('freshlist', ['freshlist']);
+var cron = require('cron');
+var scraper = require('./freshscrape');
+
 
 
 app.use(express.static(__dirname + "/public"));
@@ -14,6 +17,18 @@ app.get('/freshlist', function (req, res) {
     	res.json(docs);
     })
 });
+
+var cronJob = cron.job("0 * * * * *", function(){
+	var songs = scraper.scrape();
+	for (song in songs) { 
+		console.log(song);
+	}
+
+	
+	console.log('cron job completed');
+}); 
+cronJob.start();
+
 
 app.listen(3000);
 console.log("Server running on port 3000");
